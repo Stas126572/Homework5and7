b0ae96a58228d7f1b8ab2784a4bf3f9d1a72f199
Stas126572 <manichev_ss@mail.ru>
2024-08-28 09:56:05 +0300

    Version2

--- Main.cpp
+++ Main.cpp
@@ -9,10 +9,10 @@ void ConteinerTest(Conteiner nd)
     std::cout << "Size: " << nd.get_size() << std::endl;
     nd.push_back(3);
     std::cout << "Size: " << nd.get_size() << std::endl;
-    std::cout << "Element 2: " << nd.GIE(0) << std::endl;
+    std::cout << "Element 2: " << nd.GetElementOfIndex(0) << std::endl;
     nd.push_back(3);
     std::cout << "Size: " << nd.get_size() << std::endl;
-    std::cout << "Element 2: " << nd.GIE(0) << std::endl;
+    std::cout << "Element 2: " << nd.GetElementOfIndex(0) << std::endl;
     std::cout << "Element: " << nd.pop_back() << std::endl;
     std::cout << "Size: " << nd.get_size() << std::endl;
     std::cout << std::endl;
